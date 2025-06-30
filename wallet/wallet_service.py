f33405d92cbc6c1e8eb02f7bfe8dbfabb7fc8ce3
kitxor <capedcrusader114@gmail.com>
2025-06-30 12:33:31 +0530

    starting wallet service

--- wallet/wallet_service.py
+++ wallet/wallet_service.py
@@ -1,18 +1,34 @@
 class WalletService:
-    def __init__(self, user):
-        self.user = user
+    def __init__(self, wallet_repository):
+        self.wallet_repository = wallet_repository
+
+    def _validate_receive(self, money):
+        if money <= 0:
+            return False
+        else:
+            return True
 
     def _validate_transfer(self, money):
-        pass
+        if money <= 0 or self.wallet.current_balance <= 0 or self.wallet.current_balance < money:
+            return False
+        else:
+            return True
 
     def add_money(self, money):
-        pass
+        validation_response = self._validate_receive(money)
+        if validation_response:
+            self.wallet.current_balance += money
+            return True
+        else:
+            return False
 
     def send_money(self, amount, destination_wallet):
-        pass
+        validation_response = self._validate_transfer(amount)
+        if validation_response:
+            self.wallet.current_balance -= amount
+            destination_wallet.current_balance += amount
+            return True
+        return False
 
     def get_balance(self):
-        pass
-
-    def get_transaction_history(self):
-        pass
+        return self.wallet.current_balance
